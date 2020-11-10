bcd2dc7f36689e238be802966f0eaad319b22f23
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-10 18:33:18 +0600

    Create C.c

---
+++ C.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    double A;
+    scanf("%lf",&A);
+    if(A >= 0 && A <= 100)
+    {
+        if(A >= 0 && A <= 25.0000)
+        {
+            printf("Intervalo [0,25]\n");
+        }
+        else if(A >= 25.0001 && A <= 50.0000)
+        {
+            printf("Intervalo (25,50]\n");
+        }
+        else if(A >= 50.0001 && A <= 75.0000)
+        {
+            printf("Intervalo (50,75]\n");
+        }
+        else
+            printf("Intervalo (75,100]\n");
+    }
+    else
+    {
+     printf("Fora de intervalo\n");
+    }
+    return 0;
+}
