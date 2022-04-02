46a7f2535b7c0994280a496fc82bf6160202f165
Bilash Biswas <67077735+bilash-biswas@users.noreply.github.com>
2022-04-03 00:19:41 +0600

    Create Clojure.clj

---
+++ Clojure.clj
@@ -0,0 +1,11 @@
+(let [num1 (do (Double/parseDouble (read-line)))] 
+  (if(and (>= num1 0) (<= num1 25))
+  (println "Intervalo [0,25]"))
+  (if(and (> num1 25) (<= num1 50))
+  (println "Intervalo (25,50]"))
+  (if(and (> num1 50) (<= num1 75))
+  (println "Intervalo (50,75]"))
+  (if(and (> num1 75) (<= num1 100))
+  (println "Intervalo (75,100]"))
+  (if(or (< num1 0) (> num1 100))
+  (println "Fora de intervalo")))
